94209e46c924bb66a68c06f0f5f5458e160b1622
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-08-23 16:45:08 -0400

    Use case temperature conversion is functional.

--- use_case_temp_conv.py
+++ use_case_temp_conv.py
@@ -1,37 +1,50 @@
 # -*- coding: utf-8 -*-
 
 from decision_matrix import DecisionMatrix
+from math import isnan, nan
+
+class TempConverter:
+
+    def __init__(self, temp_to_convert):
+        self.set_temp_to_convert(temp_to_convert)
+        self._result = nan
+
+    def convert_c_to_f(self):
+        print("Converting °C to °F")
+        self._result = self._temp_to_convert * 9 / 5 + 32
+
+    def convert_c_to_k(self):
+        print("Converting °C to K")
+        self._result = self._temp_to_convert + 273.15
+
+    def convert_f_to_c(self):
+        print("Converting °F to °C")
+        self._result = (self._temp_to_convert - 32) * 5 / 9
+
+    def convert_f_to_k(self):
+        print("Converting °F to K")
+        self._result = (self._temp_to_convert + 459.67) * 5 / 9
+
+    def convert_k_to_c(self):
+        print("Converting K to °C")
+        self._result = self._temp_to_convert - 273.15
+
+    def convert_k_to_f(self):
+        print("Converting K to °F")
+        self._result = self._temp_to_convert * 9 / 5 - 459.67
+
+    def get_result(self):
+        return self._result
+
+    def set_temp_to_convert(self, temp_to_convert):
+        self._temp_to_convert = temp_to_convert
+
+input_temp = 23
+converter = TempConverter(input_temp)
 
 DEG_C = "C"
 DEG_F = "F"
 KELVIN = "K"
-
-input_temp = 23
-output_temp = input_temp
-
-def convert_c_to_f():
-    print("Converting °C to °F")
-    output_temp = input_temp * 9 / 5 + 32
-
-def convert_c_to_k():
-    print("Converting °C to K")
-    output_temp = input_temp + 273.15
-
-def convert_f_to_c():
-    print("Converting °F to °C")
-    output_temp = (input_temp - 32) * 5 / 9
-
-def convert_f_to_k():
-    print("Converting °F to K")
-    output_temp = (input_temp + 459.67) * 5 / 9
-
-def convert_k_to_c():
-    print("Converting K to °C")
-    output_temp = input_temp - 273.15
-
-def convert_k_to_f():
-    print("Converting K to °F")
-    output_temp = input_temp * 9 / 5 - 459.67
 
 input_scale = "F"
 output_scale = "K"
@@ -47,13 +60,18 @@ output_scale_axis = [
 
 dm = DecisionMatrix(input_scale_axis, output_scale_axis)
 
-dm.set_action(convert_c_to_f, 0, 1)
-dm.set_action(convert_c_to_k, 0, 2)
-dm.set_action(convert_f_to_c, 1, 0)
-dm.set_action(convert_f_to_k, 1, 2)
-dm.set_action(convert_k_to_c, 2, 0)
-dm.set_action(convert_k_to_f, 2, 1)
+dm.set_action(converter.convert_c_to_f, 0, 1)
+dm.set_action(converter.convert_c_to_k, 0, 2)
+dm.set_action(converter.convert_f_to_c, 1, 0)
+dm.set_action(converter.convert_f_to_k, 1, 2)
+dm.set_action(converter.convert_k_to_c, 2, 0)
+dm.set_action(converter.convert_k_to_f, 2, 1)
 
 dm.print_axis_values()
 dm.run()
+
+output_temp = converter.get_result()
+if isnan(converter.get_result()):
+    output_temp = input_temp
+
 print(output_temp)
