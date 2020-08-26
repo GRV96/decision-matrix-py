0a056d1fd7a8988061238b43b4ab690b247ef3c7
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2020-08-26 11:00:15 -0400

    Classes Player and PlayerContainer in a seperate file

---
+++ players.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from loop_index.loop_index import LoopIndex
+
+class Player:
+
+    def __init__(self):
+        self._points = 0
+        self.revive() # Creates attribute _is_alive.
+
+    def get_points(self):
+        return self._points
+
+    def give_points(self, points_given):
+        self._points += points_given
+
+    def is_alive(self):
+        return self._is_alive
+
+    def kill(self):
+        self._is_alive = False
+
+    def revive(self):
+        self._is_alive = True
+
+class PlayerContainer:
+
+    def __init__(self, player_num):
+        self._players = tuple(Player() for x in range(player_num))
+        self._player_count = len(self._players)
+
+    def get_player(self, player_index):
+        return self._players[player_index]
+
+    def get_player_count(self):
+        return self._player_count
+
+    def give_points(self, player_points_dict):
+        for player_index, points in player_points_dict.items():
+            self._players[player_index].give_points(points)
+
+    def print_scores(self):
+        print("SCORES")
+        player_index = LoopIndex(self._player_count)
+        while player_index.iterate():
+            i = player_index.get_value()
+            points = self._players[i].get_points()
+            print("Player " + str(i) + ": " + str(points))
+
+    def revive_the_dead(self):
+        for player in self._players:
+            player.revive()
--- use_case_2_points.py
+++ use_case_2_points.py
@@ -1,87 +1,51 @@
 # -*- coding: utf-8 -*-
 
 from decision_matrix import DecisionMatrix
-from loop_index.loop_index import LoopIndex
+from players import PlayerContainer
 
-class Player:
-
-    def __init__(self):
-        self._points = 0
-        self.revive() # Creates attribute _is_alive.
-
-    def get_points(self):
-        return self._points
-
-    def give_points(self, points_given):
-        self._points += points_given
-
-    def is_alive(self):
-        return self._is_alive
-
-    def kill(self):
-        self._is_alive = False
-
-    def revive(self):
-        self._is_alive = True
-
-players = (Player(), Player(), Player())
-
-def give_points(player_points_dict):
-    for player_index, points in player_points_dict.items():
-        players[player_index].give_points(points)
-
-def print_scores():
-    print("SCORES")
-    player_index = LoopIndex(len(players))
-    while player_index.iterate():
-        i = player_index.get_value()
-        print("Player " + str(i) + ": " + str(players[i].get_points()))
-
-def revive_the_dead():
-    for player in players:
-        player.revive()
+pc = PlayerContainer(3)
 
 player0_axis = (
-    lambda: not players[0].is_alive(),
-    lambda: players[0].is_alive())
+    lambda: not pc.get_player(0).is_alive(),
+    lambda: pc.get_player(0).is_alive())
 player1_axis = (
-    lambda: not players[1].is_alive(),
-    lambda: players[1].is_alive())
+    lambda: not pc.get_player(1).is_alive(),
+    lambda: pc.get_player(1).is_alive())
 player2_axis = (
-    lambda: not players[2].is_alive(),
-    lambda: players[2].is_alive())
+    lambda: not pc.get_player(2).is_alive(),
+    lambda: pc.get_player(2).is_alive())
 
 dm = DecisionMatrix(player0_axis, player1_axis, player2_axis)
 
-dm.set_all_actions({(1, 0, 0): lambda: give_points({0: 2}), # Players 1 and 2 die.
-                    (0, 1, 0): lambda: give_points({1: 2}), # Players 0 and 2 die.
-                    (0, 0, 1): lambda: give_points({2: 2}), # Players 0 and 1 die.
-                    (0, 1, 1): lambda: give_points({1: 1, 2: 1}), # Player 0 dies.
-                    (1, 0, 1): lambda: give_points({0: 1, 2: 1}), # Player 1 dies.
-                    (1, 1, 0): lambda: give_points({0: 1, 1: 1})}) #Player 2 dies.
+dm.set_all_actions({(1, 0, 0): lambda: pc.give_points({0: 2}), # Players 1 and 2 die.
+                    (0, 1, 0): lambda: pc.give_points({1: 2}), # Players 0 and 2 die.
+                    (0, 0, 1): lambda: pc.give_points({2: 2}), # Players 0 and 1 die.
+                    (0, 1, 1): lambda: pc.give_points({1: 1, 2: 1}), # Player 0 dies.
+                    (1, 0, 1): lambda: pc.give_points({0: 1, 2: 1}), # Player 1 dies.
+                    (1, 1, 0): lambda: pc.give_points({0: 1, 1: 1})}) #Player 2 dies.
 # No points are awarded when all players or none die.
 
 def end_round():
     dm.print_axis_values()
     dm.run()
-    print_scores()
-    revive_the_dead()
+    pc.print_scores()
+    pc.revive_the_dead()
     print()
 
 # Players 0 and 2 die.
-players[0].kill()
-players[2].kill()
+pc.get_player(0).kill()
+pc.get_player(2).kill()
 end_round()
 
 # Player 1 dies.
-players[1].kill()
+pc.get_player(1).kill()
 end_round()
 
 # No player dies.
 end_round()
 
 # All players die.
-players[0].kill()
-players[1].kill()
-players[2].kill()
+pc.get_player(0).kill()
+pc.get_player(1).kill()
+pc.get_player(2).kill()
 end_round()
